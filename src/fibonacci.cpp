11874489c439e93e957f2a8b0e8a9fa2c9e67e46
Hasti Narimanzadeh <hasti.nz@gmail.com>
2023-08-30 17:04:24 +0300

    Compute nth term in fib sequence non-recursively

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-int fib(int n) {
+int fib_recursive(int n) {
 	if (n == 0) return n;
 	if (n == 1) return n;
-	return fib(n - 1) + fib(n - 2);
+	return fib_recursive(n - 1) + fib_recursive(n - 2);
+}
+
+int fib(int n) {
+	int t1 = 1;
+	int t2 = 2;
+	int i = 1;
+	while (i < n) {
+		int temp = t1 + t2;
+		t1 = t2;
+		t2 = temp;
+		i++;
+}
+	return t1;
 }
 
 int main() {
